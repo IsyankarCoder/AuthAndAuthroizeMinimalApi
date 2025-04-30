3f50b673ed6c60ff6d347e9b8472ae20e13c2fca
Volkan Genç <gencvolkan@gmail.com>
2025-04-30 22:43:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", (HttpContext ctx) => "Hello World!");
+
+app.MapGet("/username",(HttpContext ctx,IDataProtector protector)=>{
+    var authCookie =  ctx.Request.Headers.Cookie.FirstOrDefault(x=>x.StartsWith("auth="));
+    var protectedPayload = authCookie.Split("=").Last();
+    var payload = protector.Unprotect(protectedPayload); 
+    var parts = payload.Split(":");
+    var key =  parts[0];
+    var value = parts[1]; 
+    return value; 
+});
+
+app.MapGet("/login",(HttpContext ctx,IDataProtector idp)=>{
+    var protector  = idp.CreateProtector("auth-cookie");
+    ctx.Response.Headers["set-cookie"]=$"auth={protector.Protect("usr:volkan")}";
+  return "Ok";
+});
+
+app.Run();
+
+
