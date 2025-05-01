c3e433f04d242e4102e1cbdc53a825958f23ddec
Volkan Genç <gencvolkan@gmail.com>
2025-05-01 17:43:49 +0300

    AuthService added

--- Program.cs
+++ Program.cs
@@ -1,37 +1,53 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 builder.Services.AddDataProtection(c=>{
    c.ApplicationDiscriminator="Volki Tolki";
 });
 
-builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/username",(HttpContext ctx,IDataProtectionProvider idp)=>{
-
+app.Use((ctx,next)=>{
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = idp.CreateProtector("auth-cookie");
 
     var authCookie =  ctx.Request.Headers.Cookie.FirstOrDefault(x=>x.StartsWith("auth="));
-    var protectedPayload = authCookie.Split("=").Last();
-    var payload = protector.Unprotect(protectedPayload); 
-    var parts = payload.Split(":");
-    var key =  parts[0];
-    var value = parts[1]; 
-    return value; 
+    var protectedPayload = authCookie?.Split("=").Last();
+
+    if(protectedPayload is not null)
+    { 
+       var payload = protector.Unprotect(protectedPayload); 
+       var parts = payload.Split(":");
+       var key =  parts[0];
+       var value = parts[1];  
+       var claims = new List<Claim>();
+    claims.Add(new Claim(key,value));
+
+    var identity = new ClaimsIdentity(claims);
+    ctx.User = new ClaimsPrincipal(identity);
+
+    }
+ return next();
 });
 
-app.MapGet("/login",(HttpContext ctx,IDataProtectionProvider idp)=>{
-    var protector  = idp.CreateProtector("auth-cookie");
-    ctx.Response.Headers["set-cookie"]=$"auth={protector.Protect("usr:volkan")}";
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/username",(HttpContext ctx)=>{ 
+    return ctx.User.FindFirst("usr").Value;
+  
+});
+
+app.MapGet("/login",(AuthService auth)=>{
+    auth.SignIn();
   return "Ok";
 });
 
-if(app.Environment.IsDevelopment()){
+if(!app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI(); 
 }
@@ -39,3 +55,17 @@ if(app.Environment.IsDevelopment()){
 app.Run();
 
 
+public class AuthService{
+   private readonly IDataProtectionProvider _idp;
+   private readonly IHttpContextAccessor _accessor;
+
+   public AuthService(IDataProtectionProvider Idp, IHttpContextAccessor Accessor){
+       _idp=Idp;
+       _accessor=Accessor;
+   }
+
+   public void SignIn(){
+    var protector = _idp.CreateProtector("auth-cookie");
+    _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:volkan")}";
+   }
+}
